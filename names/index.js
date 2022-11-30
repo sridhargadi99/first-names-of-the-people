05e15dd685526c91b22148cf5688a6992ae3598f
sridhar <sridhar.gadi99@gmail.com>
2022-11-30 12:15:34 +0000

    first names of the people solution

---
+++ names/index.js
@@ -0,0 +1,8 @@
+const newArray = require("../country/state/city/index.js");
+
+const newFunction = require("../utilities/utils/index.js");
+
+const getPeopleInCity = (newArray) => {
+  return newFunction(newArray);
+};
+module.exports = getPeopleInCity;
